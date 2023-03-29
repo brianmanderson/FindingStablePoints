a5896e73779529ef81cd54eb4fae14702c3c5279
brianmanderson <markba122@gmail.com>
2023-03-28 22:34:00 -0300

    superimpose the absolute sum of the gradients, find the min, should be our best point

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import SimpleITK as sitk
 import numpy as np
+from matplotlib.pyplot import plot
 from PlotScrollNumpyArrays import plot_scroll_Image
 import os
 
-
+dose_handle: sitk.Image
 dose_file = r'dose.nii.gz'
 reader = sitk.ImageFileReader()
 reader.SetFileName(dose_file)
 reader.ReadImageInformation()
-dose = reader.Execute()
-dose_np = sitk.GetArrayFromImage(dose)
+dose_handle = reader.Execute()
+dose_np = sitk.GetArrayFromImage(dose_handle)
 xxx = 1
 limit = 0.9
 max_dose = np.max(dose_np)
@@ -33,4 +34,13 @@ for index in range(bounding_boxes.shape[0]):
     bounding_box = bounding_boxes[index]
     c_start, r_start, z_start, _, _, _ = bounding_box
     c_stop, r_stop, z_stop = c_start + bounding_box[3], r_start + bounding_box[4], z_start + bounding_box[5]
-    image_cube = masked_np[z_start:z_stop, r_start:r_stop, c_start:c_stop]
+    dose_cube = dose_np[z_start:z_stop, r_start:r_stop, c_start:c_stop]
+    gradient_np = np.abs(np.gradient(dose_cube, 2))
+    super_imposed_gradient_np = np.sum(gradient_np, axis=0)
+    min_gradient = np.min(super_imposed_gradient_np)
+    min_location = np.where(super_imposed_gradient_np <= min_gradient*1.1)
+    min_z = int(z_start + min_location[0][0])
+    min_row = int(r_start + min_location[1][0])
+    min_col = int(c_start + min_location[2][0])
+    physical_location = dose_handle.TransformContinuousIndexToPhysicalPoint((min_col, min_row, min_z))
+    xx = 1
